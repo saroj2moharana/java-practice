f32e1a3af4c556b54923c25901c45472afcd3100
saroj <saroj.kumar2moharana@gmail.com>
2022-07-27 16:37:59 +0530

    Some more problems

--- src/main/java/com/skm/algo/string/MaxPalindromSubString.java
+++ src/main/java/com/skm/algo/string/MaxPalindromSubString.java
@@ -9,10 +9,28 @@ public class MaxPalindromSubString {
         String res = getMaxPalindromeSubString(str);
         System.out.println(res);
     }
-    public static String getMaxPalindromeSubString(String str){
-        for(int i=0; i<str.length(); i++){
 
+    /**
+     * This is solved by dynamic programming....
+     * @param str
+     * @return
+     */
+    public static String getMaxPalindromeSubString(String str){
+        boolean p[][] = new boolean[str.length()][str.length()];
+        int start = 0, end = 0;
+        for(int gap=0; gap<str.length(); gap++){
+            for(int i=0,j=gap; j<str.length(); i++,j++ ){
+                if(gap == 0){
+                    p[i][j] = true;
+                }else if(gap == 1){
+                    p[i][j] = str.charAt(i) == str.charAt(j);
+                }else if (str.charAt(i) == str.charAt(j) && p[i + 1][j - 1]) {
+                    start = i;
+                    end = j;
+                    p[i][j] = true;
+                }
+            }
         }
-        return "";
+        return str.substring(start+1,end);
     }
 }
