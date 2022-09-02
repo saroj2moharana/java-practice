51f23b060954e1685229b9bb886d2c1f9356296f
saroj <saroj.kumar2moharana@gmail.com>
2022-09-02 12:37:20 +0530

    binary to decimal

--- src/main/java/com/skm/algo/math/BinaryToDecimal.java
+++ src/main/java/com/skm/algo/math/BinaryToDecimal.java
@@ -5,15 +5,27 @@ package com.skm.algo.math;
  **/
 public class BinaryToDecimal {
     public static void main(String[] args) {
-        String n = "1000";//6
+        int n = 101;//5
         int r = covertToDecimal(n);
         System.out.println("Decimal Conversion of "+n+" is : "+r);
     }
-    public static int covertToDecimal(String n){
+    /*public static int covertToDecimal(String n){
         int r = 0;
         int l = n.length();
         for(int i=0; i<l; i++){
             r += Integer.parseInt(String.valueOf(n.charAt(i)))*Math.pow(2,l-1-i);
+        }
+        return r;
+    }*/
+
+    public static int covertToDecimal(int n){
+        if(n == 0 || n==1) return 1;
+        int r = 0;
+        int i = 0;
+        while(n>0){
+            int rem = n%10;
+            n = n/10;
+            r += rem*Math.pow(2,i++);
         }
         return r;
     }
