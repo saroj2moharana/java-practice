1c7c6ebfce2a9b1bc0b16a82e9f0d20b277650a7
Saroj <saorj@near.com>
2023-10-17 18:35:14 +0530

    Few more problems

--- src/main/java/com/skm/algo/linkedlist/LinkedListReverse.java
+++ src/main/java/com/skm/algo/linkedlist/LinkedListReverse.java
@@ -10,15 +10,14 @@ public class LinkedListReverse {
         //10-->20-->30-->40-->50-->null
         SingleListNode head = ListUtil.getDefaultSingleListNode();
         System.out.println("Normal : "+ head);
+        //SingleListNode revList = reverseListWithoutRecursion(head);
         SingleListNode revList = reverseWithRecursion(head);
-        System.out.println("reverse : "+ revList);
+        ListUtil.displayNodes(revList);
     }
-    public static SingleListNode reverseListWithoutRecursion(SingleListNode head){
-        SingleListNode nextNode = null;
-        SingleListNode curNode = head;
+    public static SingleListNode reverseListWithoutRecursion(SingleListNode curNode){
         SingleListNode prevNode = null;
         while(curNode != null){
-            nextNode = curNode.nextNode;
+            SingleListNode nextNode = curNode.nextNode;
             curNode.nextNode = prevNode;
             prevNode = curNode;
             curNode = nextNode;
@@ -26,7 +25,7 @@ public class LinkedListReverse {
         //return prevNode because we have already reached curNode as null so prevNode will be head of this linkedList...
         return prevNode;
     }
-    public static SingleListNode reverseWithRecursion(SingleListNode node){
+    /*public static SingleListNode reverseWithRecursion(SingleListNode node){
         //base condition...
         if(node.nextNode == null) return node;
         SingleListNode head = reverseWithRecursion(node.nextNode);
@@ -40,5 +39,15 @@ public class LinkedListReverse {
 
         //now return head pointer...
         return head;
+    }*/
+
+    //short form of above method...
+    public static SingleListNode reverseWithRecursion(SingleListNode head){
+        if(head.nextNode == null) return head;
+        SingleListNode prev = reverseListWithoutRecursion(head.nextNode);
+        SingleListNode temp = head.nextNode.nextNode;
+        head.nextNode.nextNode = head;
+        head.nextNode = temp;
+        return prev;
     }
 }
--- src/main/java/com/skm/algo/linkedlist/ReverseLinkedListWithGroups.java
+++ src/main/java/com/skm/algo/linkedlist/ReverseLinkedListWithGroups.java
@@ -2,16 +2,32 @@ package com.skm.algo.linkedlist;
 
 import com.skm.algo.util.ListUtil;
 
+import java.util.List;
+
 /**
  * @Author saroj on 23/04/22
  **/
 public class ReverseLinkedListWithGroups {
     public static void main(String[] args) {
+        //10-->20-->30-->40-->50-->null
         SingleListNode listNode = ListUtil.getDefaultSingleListNode();
-        System.out.println(listNode);
+        SingleListNode head = reverseWithGroup(listNode,4);
+        ListUtil.displayNodes(head);
 
     }
-    public SingleListNode reverseLisWithGroup(SingleListNode head, int groupSize){
-        return null;
+    public static SingleListNode reverseWithGroup(SingleListNode head, int size){
+        SingleListNode curNode = head;
+        SingleListNode prev = null;
+        SingleListNode next = null;
+        int i=0;
+        while(curNode != null && i<size){
+            next = curNode.nextNode;
+            curNode.nextNode = prev;
+            prev = curNode;
+            curNode = next;
+            i++;
+        }
+        if(next != null) head.nextNode = reverseWithGroup(curNode,size);
+        return prev;
     }
 }
